f71fafe93d0b05ed9b4c407ae0ca2f7d310ce405
djawny <djawny@op.pl>
2017-04-27 15:39:22 +0200

    new gradle version

--- app/src/main/java/com/sdaacademy/jawny/daniel/intenttest/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/intenttest/MainActivity.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.ImageView;
 
 import butterknife.BindView;
